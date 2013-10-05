808300b0e67ae2c876aa7eea9c1b67568f462cfa
James Young <james.young@turner.com>
2013-10-04 23:17:04 -0400

    added more unit tests

--- test/mocha/cli-wrapper.test.js
+++ test/mocha/cli-wrapper.test.js
@@ -6,13 +6,12 @@ var fs = require('fs'),
     childProcess = require('child_process'),
     packageData = require('../../package.json'),
     cliWrapperPath = path.join(path.dirname(fs.realpathSync(__filename)), '../../pnpm-replicate-cli.js'),
-    expectedOutput = 'Usage: pnpm-replicate http[s]://[username:password@]your-private-registry[:port] --id package-name[,package-name] [--target dbname] [--dryrun]\n\nOptions:\n  --help, -h     You are looking at it.                                                 \n  --version, -v  Show the current version installed.                                    \n  --id           A comma separated list of packages to replicate. Example: one,two,three\n  --target       The target database in your private registry to replicate to.          \n  --dryrun       Does not replicate.  Displays the curl command that would be used.     \n\n';
+    expectedOutput = 'Usage: pnpm-replicate http[s]://[username:password@]your-private-registry[:port] --id package-name[,package-name] [--target dbname] [--dryrun]\n\nOptions:\n  --help, -h     You are looking at it.                                                 \n  --version, -v  Show the current version installed.                                    \n  --id           A comma separated list of packages to replicate. Example: one,two,three\n  --target       The target database in your private registry to replicate to.            [default: \"registry\"]\n  --dryrun       Does not replicate.  Displays the curl command that would be used.       [default: false]\n\n';
 
 
-describe('Command Line Wrapper', function () {
+describe('pnpm-replicate', function () {
     it('should return the current version when called with -v', function (done) {
         childProcess.exec(cliWrapperPath + ' -v', function (error, stdout) {
-
             if (error) { console.dir(error); }
 
             stdout.should.equal('pnpm-replicate v' + packageData.version + '\n');
@@ -22,7 +21,6 @@ describe('Command Line Wrapper', function () {
 
     it('should return the current version when called with --version', function (done) {
         childProcess.exec(cliWrapperPath + ' --version', function (error, stdout) {
-
             if (error) { console.dir(error); }
 
             stdout.should.equal('pnpm-replicate v' + packageData.version + '\n');
@@ -53,6 +51,24 @@ describe('Command Line Wrapper', function () {
             if (error) { console.dir(error); }
 
             stdout.should.equal(expectedOutput);
+            done();
+        });
+    });
+
+    it('should return help information when called with --id with no arguments', function (done) {
+        childProcess.exec(cliWrapperPath + ' --id', function (error, stdout) {
+            if (error) { console.dir(error); }
+
+            stdout.should.equal(expectedOutput);
+            done();
+        });
+    });
+
+    it('should not do a POST when called with --dryrun', function (done) {
+        childProcess.exec(cliWrapperPath + ' foo --id foo --dryrun', function (error, stdout) {
+            if (error) { console.log(error); }
+
+            stdout.should.contain('[NOTICE] This was a dry run, no packages were replicated.');
             done();
         });
     });
